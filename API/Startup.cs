1fce1c0dff9b70725a75e1735e50c0611db8f404
asdfasdf850 <menyazovutartem9@gmail.com>
2020-10-23 09:59:40 +0300

    end of section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,13 +1,13 @@
+using API.Extensions;
 using API.Helpers;
+using API.Middleware;
 using AutoMapper;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace API
 {
@@ -22,20 +22,22 @@ namespace API
 
       public void ConfigureServices(IServiceCollection services)
       {
-         services.AddScoped<IProductRepository, ProductRepository>();
-         services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
          services.AddAutoMapper(typeof(MappingProfiles));
          services.AddControllers();
          services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+         services.AddApplicationServices();
+         services.AddSwaggerDocumentation();
       }
 
       public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
       {
-         if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+         app.UseMiddleware<ExceptionMiddleware>();
+         app.UseStatusCodePagesWithReExecute("/errors/{0}");
          app.UseHttpsRedirection();
          app.UseRouting();
          app.UseStaticFiles();
          app.UseAuthorization();
+         app.UseSwaggerDocumentation();
          app.UseEndpoints(endpoints => endpoints.MapControllers());
       }
    }
