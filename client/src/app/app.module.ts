c04508bea68574b2450f03d201a6e272d2246444
asdfasdf850 <menyazovutartem9@gmail.com>
2020-10-25 21:55:24 +0300

    end of section 9

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { NavBarComponent } from './nav-bar/nav-bar.component'
+import { CoreModule } from './core/core.module'
+import { ShopModule } from './shop/shop.module'
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoreModule,
+    ShopModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
